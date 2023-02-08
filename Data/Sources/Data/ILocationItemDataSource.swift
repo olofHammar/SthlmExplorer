7a3ad1331c5545afb788d935544eac98df18d10c
Olof Hammar <olof.hammar@shortcut.no>
2023-02-08 16:49:25 +0100

    Added test data to CardList.json

--- Data/Sources/Data/ILocationItemDataSource.swift
+++ Data/Sources/Data/ILocationItemDataSource.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
-//  
+//  ILocationItemDataSource.swift
+//  SthlmExplorer
 //
 //  Created by Olof Hammar on 2023-02-08.
 //
 
+import Combine
 import Foundation
+import Model
+
+public protocol ILocationItemDataSource {
+    func getLocationItems() -> AnyPublisher<[LocationItem], Never>
+}
