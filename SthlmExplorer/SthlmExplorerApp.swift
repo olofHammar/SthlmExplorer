19ec00c82fc2cecfbd9afd353e72f03ecdc7f0da
Olof Hammar <olof.hammar@shortcut.no>
2023-02-08 15:20:22 +0100

    Initial commit

---
+++ SthlmExplorer/SthlmExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SthlmExplorerApp.swift
+//  SthlmExplorer
+//
+//  Created by Olof Hammar on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct SthlmExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
