07f5e14a7f9ca1cd58829917a401f30baadd35f7
Olof Hammar <olof.hammar@shortcut.no>
2023-02-10 09:40:00 +0100

    Added ModalViewRouter to handle modal presentation

--- Navigation/Sources/Navigation/Routers/ModalViewRouter.swift
+++ Navigation/Sources/Navigation/Routers/ModalViewRouter.swift
@@ -5,5 +5,53 @@
 //  Created by Olof Hammar on 2023-02-10.
 //
 
+import Model
 import UIKit
 
+public protocol ModalPresenter {
+    func setModal(state: ModalPresentationState, type: ModalPresentationType)
+    func setModal(state: ModalPresentationState, type: ModalPresentationType, onDismiss: @escaping() -> Void)
+    func closeModal()
+}
+
+public final class ModalViewRouter: ObservableObject, ModalPresenter {
+
+    @Published var sheetPresentationState: ModalPresentationState?
+    @Published var fullScreenModalPresentationState: ModalPresentationState?
+
+    init() {}
+
+    public func setModal(state: ModalPresentationState, type: ModalPresentationType) {
+        modal(state: state, type: type, onDismiss: nil)
+    }
+
+    public func setModal(state: ModalPresentationState, type: ModalPresentationType, onDismiss: @escaping () -> Void) {
+        modal(state: state, type: type, onDismiss: onDismiss)
+    }
+
+    public func closeModal() {
+        sheetPresentationState = nil
+        fullScreenModalPresentationState = nil
+    }
+
+    private func modal(state: ModalPresentationState, type: ModalPresentationType, onDismiss: (() -> Void)? = nil) {
+        if sheetPresentationState == nil, fullScreenModalPresentationState == nil {
+            switch type {
+            case .fullScreen:
+                fullScreenModalPresentationState = state
+            case .sheet:
+                sheetPresentationState = state
+            }
+        } else {
+            closeModal()
+
+            if let onDismiss = onDismiss {
+                onDismiss()
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now().advanced(by: .milliseconds(300))) {
+                self.modal(state: state, type: type, onDismiss: onDismiss ?? nil)
+            }
+        }
+    }
+}
