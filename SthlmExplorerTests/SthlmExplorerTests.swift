74b17621ca66fbb0708262a98666204c92ebfe00
Olof Hammar <olof.hammar@shortcut.no>
2023-02-20 15:16:05 +0100

    Added tests for offset value and selected location

--- SthlmExplorerTests/SthlmExplorerTests.swift
+++ SthlmExplorerTests/SthlmExplorerTests.swift
@@ -12,41 +12,69 @@ import XCTest
 @testable import SthlmExplorer
 
 final class ListViewModelTests: XCTestCase {
-    var viewModel: ListViewModel!
 
     override func setUp() {
         super.setUp()
-        viewModel = ListViewModel()
     }
 
     func test_list_items_is_equal_to_static_data_after_starting_oberservers() {
         // Given
+        let vm = makeSUT()
         let expectation = XCTestExpectation(description: "List items should be equal to the combined count of CardList.json and TravelTip.json.")
 
         // When
-        viewModel.startListItemObservers()
+        vm.startListItemObservers()
 
-        let timeout = TimeInterval(2)
+        let timeout = TimeInterval(1)
         let listItemCount = 3
 
         // Then
         DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
-            XCTAssertEqual(self.viewModel.listItems.count, listItemCount)
+            XCTAssertEqual(vm.listItems.count, listItemCount)
             expectation.fulfill()
         }
 
         wait(for: [expectation], timeout: timeout)
     }
 
-    func test_favorite_location_binding() {
+    func test_favorite_location_binding_is_toggled_correctly() {
         // Given
+        let vm = makeSUT()
         let locationItem = LocationItem(location: Location.mockLocation, isFavorite: false)
         let expectedFavoriteValue = true
 
         // When
-        viewModel.favoriteBinding(locationItem).wrappedValue = expectedFavoriteValue
+        vm.favoriteBinding(locationItem).wrappedValue = expectedFavoriteValue
 
         // Then
-        XCTAssertTrue(viewModel.isFavoriteLocation(locationItem.id))
+        XCTAssertTrue(vm.isFavoriteLocation(locationItem.id))
+    }
+
+    func test_header_offset_value_is_initialised_correctly() {
+        // Given
+        let vm = makeSUT()
+        let expectedOffsetValue: CGFloat = .zero
+
+        // When
+        let actualOffsetValue = vm.headerOffsetValue()
+
+        // Then
+        XCTAssertEqual(expectedOffsetValue, actualOffsetValue)
+    }
+
+    func test_selected_location_is_set_when_detail_is_presented() {
+        // Given
+        let vm = makeSUT()
+        let selectedLocation = LocationItem(location: .mockLocation, isFavorite: false)
+
+        // When
+        vm.presentDetail(for: selectedLocation)
+
+        // Then
+        XCTAssertEqual(selectedLocation, vm.selectedLocation)
+    }
+
+    private func makeSUT() -> ListViewModel {
+        ListViewModel()
     }
 }
